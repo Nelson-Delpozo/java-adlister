71037ad2a6b8ba8f98aea0e55c1f7b02a7006be4
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-05 13:31:33 -0500

    created database

--- src/main/webapp/adlister_db.sql
+++ src/main/webapp/adlister_db.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE IF NOT EXISTS adlister_db;
 USE adlister_db;
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL ON adlister_db.* TO 'admin'@'localhost';
 
 CREATE TABLE users
 (
-    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(50),
     email    VARCHAR(50),
     password VARCHAR(50),
@@ -15,7 +13,7 @@ CREATE TABLE users
 CREATE TABLE ads
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    user_id INT UNSIGNED DEFAULT NULL,
     title VARCHAR(50),
     description VARCHAR(150),
     PRIMARY KEY (id),
