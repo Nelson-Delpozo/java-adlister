8f456928ee30a6b3ec112b566c02e3b89cae8fe4
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-06 14:45:18 -0500

    making it

--- src/main/java/Ad.java
+++ src/main/java/Ad.java
@@ -1,17 +1,17 @@
 public class Ad {
-    private long id;
-    private long userId;
+    private int id;
+    private int userId;
     private String title;
     private String description;
 
-    public Ad(long id, long userId, String title, String description) {
+    public Ad(int id, int userId, String title, String description) {
         this.id = id;
         this.userId = userId;
         this.title = title;
         this.description = description;
     }
 
-    public Ad(long userId, String title, String description) {
+    public Ad(int userId, String title, String description) {
         this.userId = userId;
         this.title = title;
         this.description = description;
@@ -21,7 +21,7 @@ public class Ad {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -29,7 +29,7 @@ public class Ad {
         return userId;
     }
 
-    public void setUserId(long userId) {
+    public void setUserId(int userId) {
         this.userId = userId;
     }
 
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,81 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.DriverManager;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+    private List<Ad> ads;
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword());
+
+    }
+
+
+    @Override
+    public List<Ad> all() throws SQLException {
+        Statement stmt;
+
+       stmt = connection.createStatement();
+       ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+       return createAdsFromResults(rs);
+
+    }
+
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+
+        List<Ad> ads = new ArrayList<>();
+
+        while (rs.next()){
+            ads.add(extractAd(rs));
+
+        }
+        return ads;
+    }
+
+    private Ad extractAd(ResultSet rs) throws SQLException {
+        return new Ad(
+                rs.getInt("id"),
+                rs.getInt("user_id"),
+                rs.getString("title"),
+                rs.getString("description")
+        );
+    }
+
+    @Override
+    public Long insert(Ad ad) throws SQLException {
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = statement.getGeneratedKeys();
+        rs.next();
+        return rs.getLong(1);
+    }
+
+    private String createInsertQuery(Ad ad) {
+        return "INSERT INTO ads(user_id, title, description) VALUES "
+                + "(" + ad.getUserId() + ", "
+                + "'" + ad.getTitle()
+                + "'" + ad.getDescription() + "')";
+    }
+
+//    private List<Ad> generateAds() throws SQLException {
+//        List<Ad> ads = new ArrayList<>();
+//        String selectQuery = "SELECT * FROM ads";
+//        Statement stmt = connection.createStatement();
+//        ResultSet rs = stmt.executeQuery(selectQuery);
+//
+//        while (rs.next()) {
+//            System.out.println("rs.getString(\"title\") = " + rs.getString("title"));
+//
+//        }
+//        return null;
+//    }
+
+}
